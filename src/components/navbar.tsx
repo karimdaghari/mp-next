5e4d9495019646f5ec625a3e16e1fd486df04385
Karim Daghari <hi@karimdaghari.com>
2024-05-26 12:51:29 +0100

    style: remove wrappers

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -17,52 +17,44 @@ import { TypographyMuted, TypographySmall } from '~/components/ui/typography';
 export function Navbar() {
   return (
     <nav className='p-2 px-4 bg-white border-b flex items-center justify-between shadow-sm'>
-      <div className='flex items-center space-x-2'>
-        <Link href='/dashboard/agendas'>
-          <Logo
-            className={buttonVariants({
-              variant: 'ghost',
-              className: 'h-10 w-fit'
-            })}
-          />
-        </Link>
-      </div>
-      <div className='pt-0.5'>
-        <div>
-          <DropdownMenu>
-            <DropdownMenuTrigger
-              className={buttonVariants({
-                variant: 'ghost',
-                className: 'flex items-center justify-start'
-              })}>
-              <div className='leading-snug text-right'>
-                <TypographySmall>Dauphine</TypographySmall>
-                <TypographyMuted className='text-xs hidden lg:block'>
-                  Jean Michel Da Silva Da Costa
-                </TypographyMuted>
-              </div>
-              <Avatar className='ml-2 h-8 w-8'>
-                <AvatarImage src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXia5ZUF0lUUa3JrMJzVQ2r-ojR8D6E9tZnd6D-6teRQ&s' />
-                <AvatarFallback>
-                  <TypographySmall>D</TypographySmall>
-                </AvatarFallback>
-              </Avatar>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align='end'>
-              <DropdownMenuLabel>
-                Jean Michel Da Silva Da Costa
-              </DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Paramètres</DropdownMenuItem>
-              <DropdownMenuItem>Support</DropdownMenuItem>
-              <DropdownMenuItem>
-                <LogOutIcon className='h-4 w-4 mr-2' />
-                Se déconnecter
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
-      </div>
+      <Link href='/dashboard/agendas'>
+        <Logo
+          className={buttonVariants({
+            variant: 'ghost',
+            className: 'h-10 w-fit'
+          })}
+        />
+      </Link>
+      <DropdownMenu>
+        <DropdownMenuTrigger
+          className={buttonVariants({
+            variant: 'ghost',
+            className: 'flex items-center justify-start pt-0.5'
+          })}>
+          <div className='leading-snug text-right'>
+            <TypographySmall>Dauphine</TypographySmall>
+            <TypographyMuted className='text-xs hidden lg:block'>
+              Jean Michel Da Silva Da Costa
+            </TypographyMuted>
+          </div>
+          <Avatar className='ml-2 h-8 w-8'>
+            <AvatarImage src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXia5ZUF0lUUa3JrMJzVQ2r-ojR8D6E9tZnd6D-6teRQ&s' />
+            <AvatarFallback>
+              <TypographySmall>D</TypographySmall>
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align='end'>
+          <DropdownMenuLabel>Jean Michel Da Silva Da Costa</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Paramètres</DropdownMenuItem>
+          <DropdownMenuItem>Support</DropdownMenuItem>
+          <DropdownMenuItem>
+            <LogOutIcon className='h-4 w-4 mr-2' />
+            Se déconnecter
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </nav>
   );
 }
